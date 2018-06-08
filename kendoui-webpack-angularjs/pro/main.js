6d662dec2859dc1b50e2a69b699c4e8c6345581b
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2018-06-08 23:24:22 +0300

    chore(minification): add imlicit $inject feat(kendo): include kendo.popup

--- kendoui-webpack-angularjs/pro/main.js
+++ kendoui-webpack-angularjs/pro/main.js
@@ -1,23 +1,28 @@
 angular = require("angular");
 
+require("@progress/kendo-ui/js/kendo.popup");
 require("@progress/kendo-ui/js/kendo.multiselect.js");
 require("@progress/kendo-ui/js/kendo.angular.js");
 require("@progress/kendo-ui/css/web/kendo.bootstrap-v4.min.css");
 
 angular.module("KendoDemos", ["kendo.directives"])
-    .controller("MyCtrl", function ($scope) {
-        $scope.selectOptions = {
-            placeholder: "Select products...",
-            dataTextField: "ProductName",
-            dataValueField: "ProductID",
-            dataSource: {
-                transport: {
-                    read: {
-                        url: "http://demos.telerik.com/kendo-ui/service/products",
-                        dataType: "jsonp"
-                    }
+    .controller("MyCtrl", MyCtrl);
+
+MyCtrl.$inject = ['$scope'];
+
+function MyCtrl($scope) {
+    $scope.selectOptions = {
+        placeholder: "Select products...",
+        dataTextField: "ProductName",
+        dataValueField: "ProductID",
+        dataSource: {
+            transport: {
+                read: {
+                    url: "http://demos.telerik.com/kendo-ui/service/products",
+                    dataType: "jsonp"
                 }
             }
-        };
-        $scope.selectedItems = [{ ProductName: "Chai", ProductID: 1 }];
-    });
+        }
+    };
+    $scope.selectedItems = [{ ProductName: "Chai", ProductID: 1 }];
+}
